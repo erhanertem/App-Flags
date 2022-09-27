689206e4f8d4086fc418496fc8cc70a50bff95e4
Erhan Ertem <eertem@gmail.com>
2022-09-27 17:01:21 +0300

    Turn the code into a function to call for different countries

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const getCountryData = function (country) {
+  //-->OLD WAY OF DOING REQUESTS - XML REQUESTS
+  //-->#1.Create a new request object
+  const request = new XMLHttpRequest();
+  //-->#2.Defines a request to the 3rd part API entry point
+  request.open('GET', `https://restcountries.com/v2/name/${country}`); //open request
+  //-->#3.Sends the request for fetching data
+  request.send();
+  // console.log(request);
 
-//-->OLD WAY OF DOING REQUESTS - XML REQUESTS
-//-->#1.Create a new request object
-const request = new XMLHttpRequest();
-//-->#2.Defines a request to the 3rd part API entry point
-request.open('GET', 'https://restcountries.com/v2/name/portugal'); //open request
-//-->#3.Sends the request for fetching data
-request.send();
-// console.log(request);
-
-request.addEventListener('load', function () {
-  // console.log(this.responseText);
-  //-->#1.Turn received JSON data to readable javascript object
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
-  //-->#2.Prep HTML fragment based on the information received
-  const html = `
+  request.addEventListener('load', function () {
+    // console.log(this.responseText);
+    //-->#1.Turn received JSON data to readable javascript object
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    //-->#2.Prep HTML fragment based on the information received
+    const html = `
   <article class="country">
     <img class="country__img" src="${data.flag}" />
     <div class="country__data">
@@ -34,7 +34,13 @@ request.addEventListener('load', function () {
     </div>
   </article>
   `;
-  //-->#3.Insert the HTML fragment into HTML body
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-});
+    //-->#3.Insert the HTML fragment into HTML body
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('portugal');
+getCountryData('usa');
+getCountryData('turkiye');
+getCountryData('germany');
