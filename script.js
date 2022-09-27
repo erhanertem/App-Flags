4489a783cb79ea5e22f2d4a939bcf453159a3621
Erhan Ertem <eertem@gmail.com>
2022-09-27 16:55:45 +0300

    Add HTML markup based on the received content from the web API

--- script.js
+++ script.js
@@ -16,6 +16,25 @@ request.send();
 
 request.addEventListener('load', function () {
   // console.log(this.responseText);
+  //-->#1.Turn received JSON data to readable javascript object
   const [data] = JSON.parse(this.responseText);
   console.log(data);
+  //-->#2.Prep HTML fragment based on the information received
+  const html = `
+  <article class="country">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1_000_000
+      ).toFixed(1)} M people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+  //-->#3.Insert the HTML fragment into HTML body
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
 });
